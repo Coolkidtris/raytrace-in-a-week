b5ac4ae5327f3234d2163af95ca394f28f39100e
Tristan COWEN <Tristancowen@hotmail.com>
2025-08-27 13:32:43 +0100

    #2
    Finished Implementation of Vector Math functions.
    Currently implementing Utility functions such as Dot and Cross products.

--- Vectors.c
+++ Vectors.c
@@ -1,38 +1,94 @@
 #include <stdio.h>
 #include "Vectors.h"
 
-Vec3 neg(Vec3 *vectorToNegate){
-    
+Vec3 *neg(Vec3 *vectorToNegate){
+    vectorToNegate->x = -vectorToNegate->x;
+    vectorToNegate->y = -vectorToNegate->y;
+    vectorToNegate->z = -vectorToNegate->z;
+    return vectorToNegate;
 }             // Negates all members of a vector
 
-Vec3 mul(Vec3 *vectorToMul, float amnt){
+Vec3 *mul(Vec3 *vectorToMul, float amnt){
+    vectorToMul->x += amnt;
+    vectorToMul->y += amnt;
+    vectorToMul->z += amnt;
 
+    return vectorToMul;
 }    // Multiplies all members of a Vector by a certain number
 
-Vec3 add(Vec3 *vectorToAdd, float amnt){
+Vec3 *div(Vec3 *vectorToDiv, float amnt){
+    vectorToDiv->x /= amnt;
+    vectorToDiv->y /= amnt;
+    vectorToDiv->z /= amnt;
 
+    return vectorToDiv;
+}
+
+Vec3 *add(Vec3 *vectorToAdd, float amnt){
+    vectorToAdd->x += amnt;
+    vectorToAdd->y += amnt;
+    vectorToAdd->z += amnt;
+
+    return vectorToAdd;
 }    // Adds a value to all members of a Vector
 
 // Math Functions - Vectorised
-Vec3 vadd(Vec3 *v1, Vec3 *v2){
+Vec3 vadd(Vec3 v1, Vec3 v2){
+    Vec3 newVec;
 
-}              // Adds 2 vectors together
+    newVec.x = v1.x + v2.x;
+    newVec.y = v1.y + v2.y;
+    newVec.z = v1.z + v2.z;
 
-Vec3 vsub(Vec3 *v1, Vec3 *v2){
+    return newVec;
+}              // Adds 2 vectors together and returns a third
 
+Vec3 vsub(Vec3 v1, Vec3 v2){
+    Vec3 newVec;
+
+    newVec.x = v1.x - v2.x;
+    newVec.y = v1.y - v2.y;
+    newVec.z = v1.z - v2.z;
+
+    return newVec;
 }              // Subtract 2 vectors from each other
 
-Vec3 vmul(Vec3 *v1, Vec3 *v2){
+Vec3 vmul(Vec3 v1, Vec3 v2){
+    Vec3 newVec;
 
+    newVec.x = v1.x * v2.x;
+    newVec.y = v1.y * v2.y;
+    newVec.z = v1.z * v2.z;
+
+    return newVec;
 }              // Multiply 2 vectors together
 
-Vec3 vdiv(Vec3 *v1, Vec3 *v2){
+Vec3 vdiv(Vec3 v1, Vec3 v2){
+    Vec3 newVec;
 
+    newVec.x = v1.x / v2.x;
+    newVec.y = v1.y / v2.y;
+    newVec.z = v1.z / v2.z;
+
+    return newVec;
 }              // Divide 2 vectors by each other
 
 // Utility Functions
-void vout(Vec3 vector){}                     // Prints all members of a Vector
+void vout(Vec3 vector){
+    return printf("%f, %f, %f", vector.x, vector.y, vector.z);
+}                     // Prints all members of a Vector
+
+Vec3 *sqr(Vec3 *vectorToSqr){
+    vectorToSqr->x *= vectorToSqr->x;
+    vectorToSqr->y *= vectorToSqr->y;
+    vectorToSqr->z *= vectorToSqr->z;
+
+    return vectorToSqr;
+}                // Squares all members of a vector
+
 float vlen(Vec3 vector){}                    // Returns the length of a Vector
-Vec3 sqr(Vec3 *vectorToSqr){}                // Squares all members of a vector
-Vec3 dot(Vec3 *vector){}                     // Returns a Vector with its dot product
+float dot(Vec3 v1, Vec3 v2){
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; 
+}                     // Returns a Vector with its dot product
+
 Vec3 cross(Vec3 *vector){}                   // Returns a Vector with its cross product
--- Vectors.h
+++ Vectors.h
@@ -25,20 +25,21 @@ typedef struct{
 }Vec4;
 
 // Math Functions - Scalar
-Vec3 neg(Vec3 *vectorToNegate);             // Negates all members of a vector
-Vec3 mul(Vec3 *vectorToMul, float amnt);    // Multiplies all members of a Vector by a certain number
-Vec3 add(Vec3 *vectorToAdd, float amnt);    // Adds a value to all members of a Vector
+Vec3* neg(Vec3 *vectorToNegate);             // Negates all members of a vector
+Vec3* mul(Vec3 *vectorToMul, float amnt);    // Multiplies all members of a Vector by a certain number
+Vec3 *div(Vec3 *vectorToDiv, float amnt);
+Vec3* add(Vec3 *vectorToAdd, float amnt);    // Adds a value to all members of a Vector
 
 // Math Functions - Vectorised
-Vec3 vadd(Vec3 *v1, Vec3 *v2);              // Adds 2 vectors together
-Vec3 vsub(Vec3 *v1, Vec3 *v2);              // Subtract 2 vectors from each other
-Vec3 vmul(Vec3 *v1, Vec3 *v2);              // Multiply 2 vectors together
-Vec3 vdiv(Vec3 *v1, Vec3 *v2);              // Divide 2 vectors by each other
+Vec3 vadd(Vec3 v1, Vec3 v2);                // Adds 2 vectors together
+Vec3 vsub(Vec3 v1, Vec3 v2);                // Subtract 2 vectors from each other
+Vec3 vmul(Vec3 v1, Vec3 v2);                // Multiply 2 vectors together
+Vec3 vdiv(Vec3 v1, Vec3 v2);                // Divide 2 vectors by each other
 
 // Utility Functions
 void vout(Vec3 vector);                     // Prints all members of a Vector
 float vlen(Vec3 vector);                    // Returns the length of a Vector
-Vec3 sqr(Vec3 *vectorToSqr);                // Squares all members of a vector
-Vec3 dot(Vec3 *vector);                     // Returns a Vector with its dot product
-Vec3 cross(Vec3 *vector);                   // Returns a Vector with its cross product
+Vec3 *sqr(Vec3 *vectorToSqr);               // Squares all members of a vector
+float dot(Vec3 v1, Vec3 v2);                // Returns a Vector with its dot product
+Vec3 cross(Vec3 vector);                    // Returns a Vector with its cross product
 #endif
